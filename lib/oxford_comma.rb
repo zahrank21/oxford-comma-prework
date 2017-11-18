909fbb4eb5d171273fffccc818d90bcae8156155
zahrank21 <zahrank@gmail.com>
2017-11-18 09:12:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.length == 1
     return array[0]
   elsif array.length == 2
-    return array[0] + "," + " and " + array[-1]
+    return array[0] +  " and " + array[-1]
   elsif array.length > 2
   array.each_with_index {|el, idx|
   if idx == array.length - 1
