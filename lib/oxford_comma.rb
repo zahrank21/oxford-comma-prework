8d6e558ac532be63096a93f12311d2132a324379
zahrank21 <zahrank@gmail.com>
2017-11-18 09:11:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,17 @@
-# def oxford_comma(array)
-#   str = ""
-#   if array.length == 1
-#     return arr[0]
-#   elsif array.length == 2
-#     return arr[0] + "," + " and " + arr[-1]
-#   else
-#   array.each_with_index {|el, idx|
-#   if idx == -1
-#     str << "and " + array[idx]
-#   else
-#     str << array[idx] + ","
-#   end
-#   }
-#   end
-#   str
-# end
+def oxford_comma(array)
+  str = ""
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array[0] + "," + " and " + array[-1]
+  elsif array.length > 2
+  array.each_with_index {|el, idx|
+  if idx == array.length - 1
+    str << "and " + array[idx]
+  else
+    str << array[idx] + ", "
+  end
+  }
+  end
+  str
+end
